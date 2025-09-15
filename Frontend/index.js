648d683bdaadeaa29427a936ee5ee2960bca375f
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2025-09-15 16:47:33 -0300

    novo

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,11 +1,17 @@
-let div_teste = document.getElementById("teste");
+let lista_produtos = document.getElementById("lista_produtos");
 
 fetch('http://localhost:5000/produtos')
 
     .then(response => response.json())
 
-    .then(data => {
-        console.log(data)
+    .then(produtos => {
+        produtos.forEach(produto => {
+            lista_produtos.innerHTML += `<li class="list-group-item">Id: ${produto.id}
+            <div class="d-flex justify-content-between">
+            <h4>Nome: ${produto.nome}  <br>  Quantidade: ${produto.quantidade}</h4>
+            </div>
+            </li>`
+        });
     })
 
     .catch(error => console.log(error));
