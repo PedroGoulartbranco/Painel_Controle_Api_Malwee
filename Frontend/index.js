03f789ffbd7c63bd5b2fd82e31e6ece721071f3e
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2025-09-17 15:54:27 -0300

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -18,7 +18,7 @@ function carregar_produtos() {
             <div>
             <input type="number" class="form-control" id="numero_quantidade_${produto.id}" >
             <button type="button" class="btn btn-primary" onclick="aumentar_quantidade('${produto.id}')">Aumentar</button>
-            <button type="button" class="btn btn-warning" onclick="aumentar_quantidade('${produto.id}')">Diminuir Fazer</button>
+            <button type="button" class="btn btn-warning" onclick="diminuir_quantidade('${produto.id}')">Diminuir</button>
             </div>
             </div>
             </li>
@@ -29,10 +29,21 @@ function carregar_produtos() {
     .catch(error => console.log(error));
 }
 
-
+function verificar_numero_positvo(id) {
+    let numero_digitado = document.getElementById(`numero_quantidade_${id}`).value;
+    if (numero_digitado > 0) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function aumentar_quantidade(id) {
-        let numero_digitado = document.getElementById(`numero_quantidade_${id}`).value;
+        let valor_valido = verificar_numero_positvo(id);
+        if (!valor_valido) {
+            alert("Por favor, digite um número acima de 0 por favor!")
+        } else {
+            let numero_digitado = document.getElementById(`numero_quantidade_${id}`).value;
         numero_digitado = parseInt(numero_digitado)
     
         let indice_produto_escolhido = todos_produtos.findIndex(produto_achar => produto_achar.id == id)
@@ -63,6 +74,63 @@ function aumentar_quantidade(id) {
     
         .catch(error => console.log(error));
     }
+        }
+
+function verificar_se_pode_diminuir(id) {
+    let numero_digitado = document.getElementById(`numero_quantidade_${id}`).value;
+    let indice_produto = todos_produtos.findIndex(produto_achar => produto_achar.id == id);
+    produto_atual = todos_produtos[indice_produto];
+
+    if (numero_digitado < 0) {
+        numero_digitado *= -1
+    }
+    if (numero_digitado > produto_atual.quantidade) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function diminuir_quantidade(id) {
+    let pode_diminuir = verificar_se_pode_diminuir(id);
+    if (!pode_diminuir) {
+        alert("Por favor, digite um número menor!")
+    } else {
+        let numero_digitado = document.getElementById(`numero_quantidade_${id}`).value;
+    let indice_produto = todos_produtos.findIndex(produto_achar => produto_achar.id == id);
+    let produto_escolhido = todos_produtos[indice_produto];
+    if (numero_digitado < 0) {
+        numero_digitado *= -1;
+    }
+    fetch(`http://localhost:5000/produtos/${id}`, {
+    
+        method: 'PUT',
+    
+        headers: {
+    
+            'Content-Type': 'application/json'
+    
+        },
+    
+        body: JSON.stringify({
+            id: produto_escolhido.id,
+            nome: produto_escolhido.nome,
+            quantidade: produto_escolhido.quantidade - numero_digitado
+        })
+    
+    })
+    
+        .then(response => response.json())
+    
+        .then(data => {
+            carregar_produtos();
+        })
+    
+        .catch(error => console.log(error));
+    }
+
+    
+}
 
 window.onload = function () {
         carregar_produtos(); 
